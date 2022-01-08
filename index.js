2a6ae1751db8a08ef4c79788833cb84f5c50d4a9
Rodolfo <rudolfrce@gmail.com>
2022-01-08 10:43:58 -0700

    Add game round and score feature to my game

--- index.js
+++ index.js
@@ -20,5 +20,25 @@ let playRound = (playerSelection) => {
       : result == 1
       ? "Human Wins"
       : "Computer Wins";
-  return winner + `. Computer move was ${computerMove.toUpperCase()}`;
+  return winner + ". Rival moves " + computerMove.toUpperCase();
+};
+
+let game = () => {
+  let scoreHuman = 0,
+    scoreComputer = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = String(prompt("Choose Rock, Paper or Scissors:"));
+    let gameRound = playRound(playerSelection);
+    scoreHuman = gameRound.match(/human/i) != null ? ++scoreHuman : scoreHuman;
+    scoreComputer =
+      gameRound.match(/computer/i) != null ? ++scoreComputer : scoreComputer;
+    console.log(gameRound);
+  }
+  let finalScore =
+    scoreHuman > scoreComputer
+      ? `Human Wins ${scoreHuman}-${scoreComputer}`
+      : scoreHuman < scoreComputer
+      ? `Computer Wins ${scoreComputer}-${scoreHuman}`
+      : `It's a Tie to ${scoreHuman}`;
+  return finalScore;
 };
